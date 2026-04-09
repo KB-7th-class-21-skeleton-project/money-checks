0cd64c5b7ffcca6155af6783d30f2946e625423a
김지민 <kimjm9841@naver.com>
2026-04-09 10:29:17 +0900

    feat: 거래 내역 상세 페이지 구현

--- src/api/account.js
+++ src/api/account.js
@@ -56,3 +56,47 @@ export const patchAccount = ({ accountId, data }) => {
 export const deleteAccount = (accountId) => {
 	return api.delete(`/account/${accountId}`);
 };
+
+const BASE = "http://localhost:3000";
+
+const http = async (url, options = {}) => {
+	const res = await fetch(`${BASE}${url}`, {
+		headers: { "Content-Type": "application/json" },
+		...options,
+	});
+	if (!res.ok) throw new Error(`HTTP ${res.status}`);
+	return res.json();
+};
+
+// 유저 단건 (id로)
+export const getUser = (id) => http(`/users/${id}`);
+
+// 유저 목록
+export const getUsers = () => http("/users");
+
+// 가계부 단건
+export const getAccount = (id) => http(`/account/${id}`);
+
+// 댓글 목록 (accountId 기준)
+export const getComments = (accountId) => http(`/comments?accountId=${accountId}`);
+
+// 댓글 작성
+export const createComment = (data) =>
+	http("/comments", { method: "POST", body: JSON.stringify(data) });
+
+// 댓글 수정
+export const updateComment = (id, data) =>
+	http(`/comments/${id}`, { method: "PATCH", body: JSON.stringify(data) });
+
+// 댓글 삭제
+export const deleteComment = (id) => http(`/comments/${id}`, { method: "DELETE" });
+
+// 리액션 목록 (accountId 기준)
+export const getReactions = (accountId) => http(`/reactions?accountId=${accountId}`);
+
+// 리액션 추가
+export const createReaction = (data) =>
+	http("/reactions", { method: "POST", body: JSON.stringify(data) });
+
+// 리액션 삭제
+export const deleteReaction = (id) => http(`/reactions/${id}`, { method: "DELETE" });
