fe34028ec0451da304a63bfc830fe667d9510b9b
itleo29 <itlei2966@gmail.com>
2026-04-10 16:45:13 +0900

    Feat: plus버튼 추가

--- src/views/Account/page.vue
+++ src/views/Account/page.vue
@@ -2,6 +2,7 @@
 import TransactionList from "@/views/Account/components/TransactionList.vue";
 import TransactionFilter from "./components/TransactionFilter.vue";
 import FriendsTap from "./components/FriendsTap.vue";
+import { Plus } from "lucide-vue-next";
 import { ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
@@ -53,6 +54,17 @@ watch(
 				:selectedDate="selectedDate"
 				:selectedFriend="selectedFriend"
 			/>
+		</div>
+		
+		<!-- 우측 하단 등록(플로팅) 버튼 래퍼 (콘텐츠 영역 기준 위치 고정) -->
+		<div class="fixed bottom-0 left-0 right-0 max-w-[720px] mx-auto w-full pointer-events-none z-50">
+			<router-link
+				to="/account/edit/new"
+				@click.stop
+				class="absolute bottom-[80px] right-[20px] w-[60px] h-[60px] bg-[#F4CF4D] rounded-full flex justify-center items-center shadow-lg transition-transform hover:scale-105 active:scale-95 pointer-events-auto cursor-pointer"
+			>
+				<Plus class="text-white w-[32px] h-[32px]" />
+			</router-link>
 		</div>
 	</div>
 </template>
