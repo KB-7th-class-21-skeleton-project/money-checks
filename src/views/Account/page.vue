0f0d7eff93ba270a035732592d7f9e39a585c47d
itleo29 <itlei2966@gmail.com>
2026-04-10 14:10:01 +0900

    feat: account-page-friends (#11)

--- src/views/Account/page.vue
+++ src/views/Account/page.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TransactionList from "@/views/Account/components/TransactionList.vue";
 import TransactionFilter from "./components/TransactionFilter.vue";
-
+import FriendsTap from "./components/FriendsTap.vue";
 import { ref } from "vue";
 
 const selectedCategory = ref("0");
@@ -13,21 +13,38 @@ const selectedDate = ref("0");
 const handleDateChange = (date) => {
 	selectedDate.value = date;
 };
+
+const selectedFriend = ref("1");
+const handleFriendChage = (friendId) => {
+	selectedFriend.value = friendId;
+};
 </script>
 
 <template>
-	<div class="d-flex justify-content-center min-vh-100 px-5">
+	<div class="wrapper d-flex justify-content-center min-vh-100 w-100 px-5">
 		<div class="account-header-container"></div>
 		<div class="account-main-container d-flex flex-column w-100">
+			<FriendsTap :selectedFriend="selectedFriend" @change-friend="handleFriendChage" />
 			<TransactionFilter
 				:selectedCategory="selectedCategory"
 				:selectedDate="selectedDate"
+				:selectedFriend="selectedFriend"
 				@change-date="handleDateChange"
 				@change-category="handleCategoryChange"
 			/>
-			<TransactionList :currentCategory="selectedCategory" :selectedDate="selectedDate" />
+			<TransactionList
+				:currentCategory="selectedCategory"
+				:selectedDate="selectedDate"
+				:selectedFriend="selectedFriend"
+			/>
 		</div>
 	</div>
 </template>
+
+<style>
+html {
+	scrollbar-gutter: stable;
+}
+</style>
 
 <style scoped></style>
